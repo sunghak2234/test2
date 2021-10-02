fcad89fbe445d0bf16415ac461944e7dfcfe75e1
sunghak2234 <sunghak2234@gmail.com>
2021-10-03 00:25:31 +0900

    test9

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -39,10 +39,9 @@ while True:
 SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.247.218.32:8000']
-
 
 # Application definition
 
